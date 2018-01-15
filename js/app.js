c42d15cb11cace26136eb7d7153807c9d6150972
devNimi <pancholinimesh25@gmail.com>
2018-01-15 15:59:20 +0530

    added chat interation + nav fix + line button + thailand button fix

--- js/app.js
+++ js/app.js
@@ -1,11 +1,16 @@
+
+function main() {
+
 (function () {
+   'use strict';
+
 	// Hide .navbar first
 	$(".navbar").hide();
 
 	// Fade in .navbar
 	$(function () {
 		$(window).scroll(function () {
-						// set distance user needs to scroll before we fadeIn navbar
+            // set distance user needs to scroll before we fadeIn navbar
 			if ($(this).scrollTop() > 200) {
 				$('.navbar').fadeIn();
 			} else {
@@ -19,3 +24,7 @@
 
 
 }());
+
+
+}
+main();
